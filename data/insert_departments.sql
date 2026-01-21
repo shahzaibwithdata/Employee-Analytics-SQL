2c30a2dcf79c65bcba744cdfe0c3753a0715f0fa
shahzaibwithdata <shahzaibwithdata@gmail.com>
2026-01-21 21:36:49 +0500

    Add department seed data

---
+++ data/insert_departments.sql
@@ -0,0 +1,11 @@
+USE EmployeeAnalyticsDB;
+GO
+
+INSERT INTO departments (department_id, department_name)
+VALUES
+(1, 'Engineering'),
+(2, 'Data'),
+(3, 'HR'),
+(4, 'Finance'),
+(5, 'Sales'),
+(6, 'Operations');
