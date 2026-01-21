94cdb322ea47037bb20a06a3c8e96b36d91ceee6
shahzaibwithdata <shahzaibwithdata@gmail.com>
2026-01-22 02:04:45 +0500

    Insert seed data for job titles

---
+++ data/job_titles_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO job_titles (job_title, job_level) VALUES
+('Data Analyst', 'Junior'),
+('Data Analyst', 'Mid'),
+('Senior Data Analyst', 'Senior'),
+('HR Analyst', 'Mid'),
+('Software Engineer', 'Junior'),
+('Software Engineer', 'Mid'),
+('Senior Software Engineer', 'Senior'),
+('Team Lead', 'Lead'),
+('Manager', 'Manager');
