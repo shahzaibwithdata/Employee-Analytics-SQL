62366ac0c84b22cbe628213e7751f6b04e1fd58c
shahzaibwithdata <shahzaibwithdata@gmail.com>
2026-01-23 21:21:28 +0500

    Delete data/employees_job_titles.sql

--- data/employees_job_titles.sql
+++
@@ -1,5 +0,0 @@
-UPDATE employees SET job_title_id = 1 WHERE employee_id IN (1,2,3,4);
-UPDATE employees SET job_title_id = 2 WHERE employee_id IN (5,6,7,8);
-UPDATE employees SET job_title_id = 3 WHERE employee_id IN (9,10);
-UPDATE employees SET job_title_id = 6 WHERE employee_id IN (11,12,13);
-UPDATE employees SET job_title_id = 8 WHERE employee_id IN (14,15);
