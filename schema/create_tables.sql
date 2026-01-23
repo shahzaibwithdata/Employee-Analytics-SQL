fb6bda84607db9b7f589a1672b291fe6b7b30435
shahzaibwithdata <shahzaibwithdata@gmail.com>
2026-01-23 21:18:10 +0500

    Initial schema added

--- schema/create_tables.sql
+++ schema/create_tables.sql
@@ -4,16 +4,29 @@ GO
 USE EmployeeAnalyticsDB;
 GO
 
+-- 1️⃣ Departments
 CREATE TABLE departments (
     department_id INT PRIMARY KEY,
-    department_name VARCHAR(100) NOT NULL
+    department_name VARCHAR(50)
 );
 
+-- 2️⃣ Job Titles
+CREATE TABLE job_titles (
+    job_title_id INT PRIMARY KEY,
+    job_title VARCHAR(50),
+    job_level VARCHAR(20),
+    department_id INT,
+    CONSTRAINT fk_job_department FOREIGN KEY (department_id) REFERENCES departments(department_id)
+);
+
+-- 3️⃣ Employees
 CREATE TABLE employees (
     employee_id INT PRIMARY KEY,
-    name VARCHAR(100) NOT NULL,
+    name VARCHAR(50),
     department_id INT,
     salary INT,
     hire_date DATE,
-    FOREIGN KEY (department_id) REFERENCES departments(department_id)
+    job_title_id INT,
+    CONSTRAINT fk_employee_department FOREIGN KEY (department_id) REFERENCES departments(department_id),
+    CONSTRAINT fk_employee_job FOREIGN KEY (job_title_id) REFERENCES job_titles(job_title_id)
 );
