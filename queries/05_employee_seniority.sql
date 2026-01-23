79e9e9201b1a3ba437cdb8d957971d10292104ae
shahzaibwithdata <shahzaibwithdata@gmail.com>
2026-01-24 01:00:39 +0500

    Delete queries/05_employee_seniority.sql

--- queries/05_employee_seniority.sql
+++
@@ -1,8 +0,0 @@
--- Employee seniority based on hire date
-
-SELECT
-    name,
-    hire_date,
-    DATEDIFF(YEAR, hire_date, GETDATE()) AS years_of_experience
-FROM employees
-ORDER BY years_of_experience DESC;
